81439c690edf6d372dcf8d390566e2eef5e43af6
Garrett Trott <garretttrott@gmail.com>
2019-10-21 21:23:27 -0700

    Actually give a good example of project at home

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 var sent = prompt("enter text:");
 console.log(sent);
 
-function lastCap(str) {
-  sent.length(-1).toUpperCase()
-  return
+function firstLastCap(str) {
+  let first = str.charAt(0).toUpperCase();
+  let last = str.slice(-1).toUpperCase();
+  return first + last;
 }
+
+function rev(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+
+function flCapRev(str) {
+  return rev(firstLastCap(str));
+}
+
+function flCapRevCat(str) {
+  return str + rev(firstLastCap(str));
+}
+
+function fourth(str) {
+  return str.charAt(math.round(parseInt(str.length()) / 2)) + str;
+}
+alert(firstLastCap(sent));
+alert(rev(sent));
+alert(flCapRev(sent));
+alert(flCapRevCat(sent));
+alert(fourth(sent));
